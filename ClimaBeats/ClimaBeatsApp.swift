1d2d815ec115c4e93b2f40136ed3d6a18d9a6a67
jony2511 <141722597+jony2511@users.noreply.github.com>
2026-04-06 21:29:58 -0700

    Initail Commit for project

---
+++ ClimaBeats/ClimaBeatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaBeatsApp.swift
+//  ClimaBeats
+//
+//  Created by macos on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ClimaBeatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
